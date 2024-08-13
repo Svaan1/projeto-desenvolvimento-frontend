b142325441938ef42b7fff12ae9b1a8078f6a115
Matheus Motizuki <matheusmotizuki@gmail.com>
2024-08-12 21:55:22 -0300

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<main>
+    <img class="spotify" src="spotify.png" alt="spotify ico">
+</main>
+
+<style>
+    /* Centering container */
+    main {
+        height: 80vh; /* 80% of the viewport height */
+        display: flex;
+        justify-content: center; /* Center content horizontally */
+        align-items: center; /* Center content vertically */
+        margin: 0 auto;
+    }
+
+    /* Image styling */
+    .spotify {
+        width: 250px;
+        height: auto;
+    }
+
+</style>
