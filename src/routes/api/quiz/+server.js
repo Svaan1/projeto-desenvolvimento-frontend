7930d9be79f6ca7a02a24a92e689d1fc8f1ac9ab
Gustavo B. <101028265+adeptusgb@users.noreply.github.com>
2024-10-05 00:02:53 +0000

    fix: make connection with api directly

--- src/routes/api/quiz/+server.js
+++ src/routes/api/quiz/+server.js
@@ -1,11 +1,9 @@
-
-
 /** @type {import('./$types').RequestHandler} */
-export async function GET({  }) {
-    let request = await fetch(import.meta.env.VITE_BACKEND_URL + "api/v1/quiz");
-    let data = await request.json();
+export async function GET ({}) {
+  let request = await fetch('https://backendproject.fly.dev/api/v1/quiz')
+  let data = await request.json()
 
-    return new Response(JSON.stringify(data),
-        { headers: { "content-type": "application/json" } }
-    );
+  return new Response(JSON.stringify(data), {
+    headers: { 'content-type': 'application/json' }
+  })
 }
--- src/routes/api/search/+server.js
+++ src/routes/api/search/+server.js
@@ -1,18 +1,19 @@
-import { error } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit'
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ url }) {
-    const urlSearchParams = url.searchParams;
+export async function GET ({ url }) {
+  const urlSearchParams = url.searchParams
 
+  const uri = `https://backendproject.fly.dev/search?q=${urlSearchParams.get(
+    'q'
+  )}&t=${urlSearchParams.get('t')}`
 
-    const uri = import.meta.env.VITE_BACKEND_URL + `search?q=${urlSearchParams.get('q')}&t=${urlSearchParams.get(('t'))}`;
+  let request = await fetch(uri)
+  let data = await request.json()
 
-    let request = await fetch(uri)
-    let data = await request.json();
+  console.log(uri)
 
-    console.log(uri);
-
-    return new Response(JSON.stringify(data),
-        { headers: { "content-type": "application/json" } }
-    );
+  return new Response(JSON.stringify(data), {
+    headers: { 'content-type': 'application/json' }
+  })
 }
